d8524bf0cc0ed4f4c23074ebfb6a93de4fd83acc
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2023-01-08 20:44:13 -0500

    project 1 complete

--- weekOne-PasswordAuth/js/main.js
+++ weekOne-PasswordAuth/js/main.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('click',passwordTextAppear);
+document.querySelector('#passCheck').addEventListener('click',passwordTextAppear);
 
 
 
@@ -13,54 +13,97 @@ function passwordTextAppear(){
 }
 
 
-
-
-
-
-let password = document.querySelector('input').value
-
 console.log(password);
 
+function passwordAuth(){
 
-// Lower-case Check
-if (password === ""){
-    document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
-}else if(password !== password.toUpperCase()){
-    document.getElementById('lowerCaseCheck').style.color = 'rgb(132,204,22)'
-}else{
-    document.getElementById('lowerCaseCheck').style.color = 'rgb(185,28,28)'
+
+    let password = document.querySelector('input').value
+
+    console.log(password);
+
+            // Lower-case Check
+        if (password === ""){
+            document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
+        }else if(password !== password.toUpperCase()){
+            document.getElementById('lowerCaseCheck').style.color = 'rgb(132,204,22)'
+        }else{
+            document.getElementById('lowerCaseCheck').style.color = 'rgb(185,28,28)'
+        }
+
+
+        // Upper case check
+        if (password === ""){
+            document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
+        }else if(password !== password.toLowerCase()){
+            document.getElementById('upperCaseCheck').style.color = 'rgb(132,204,22)'
+        }else{
+            document.getElementById('upperCaseCheck').style.color = 'rgb(185,28,28)'
+        }
+
+        // Number Check
+
+        function containsNum(password){
+            return /\d/.test(password)
+        }
+
+        if (password === ""){
+            document.getElementById('numCheck').style.color = 'rgb(100 116 139)'
+        }else if(containsNum(password) === true){
+            document.getElementById('numCheck').style.color = 'rgb(132,204,22)'
+        }else{
+            document.getElementById('numCheck').style.color = 'rgb(185,28,28)'
+        }
+
+        // Character Count Check
+
+        if (password === ""){
+            document.getElementById('charCheck').style.color = 'rgb(100 116 139)'
+        }else if(password.length >= 8){
+            document.getElementById('charCheck').style.color = 'rgb(132,204,22)'
+        }else{
+            document.getElementById('charCheck').style.color = 'rgb(185,28,28)'
+        }
 }
+// // Lower-case Check
+// if (password === ""){
+//     document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
+// }else if(password !== password.toUpperCase()){
+//     document.getElementById('lowerCaseCheck').style.color = 'rgb(132,204,22)'
+// }else{
+//     document.getElementById('lowerCaseCheck').style.color = 'rgb(185,28,28)'
+// }
 
 
-// Upper case check
-if (password === ""){
-    document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
-}else if(password !== password.toLowerCase()){
-    document.getElementById('upperCaseCheck').style.color = 'rgb(132,204,22)'
-}else{
-    document.getElementById('upperCaseCheck').style.color = 'rgb(185,28,28)'
-}
+// // Upper case check
+// if (password === ""){
+//     document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
+// }else if(password !== password.toLowerCase()){
+//     document.getElementById('upperCaseCheck').style.color = 'rgb(132,204,22)'
+// }else{
+//     document.getElementById('upperCaseCheck').style.color = 'rgb(185,28,28)'
+// }
 
-// Number Check
+// // Number Check
 
-function containsNum(password){
-    return /\d/.test(password)
-}
+// function containsNum(password){
+//     return /\d/.test(password)
+// }
 
-if (password === ""){
-    document.getElementById('numCheck').style.color = 'rgb(100 116 139)'
-}else if(containsNum(password) === true){
-    document.getElementById('numCheck').style.color = 'rgb(132,204,22)'
-}else{
-    document.getElementById('numCheck').style.color = 'rgb(185,28,28)'
-}
+// if (password === ""){
+//     document.getElementById('numCheck').style.color = 'rgb(100 116 139)'
+// }else if(containsNum(password) === true){
+//     document.getElementById('numCheck').style.color = 'rgb(132,204,22)'
+// }else{
+//     document.getElementById('numCheck').style.color = 'rgb(185,28,28)'
+// }
 
-// Character Count Check
+// // Character Count Check
 
-if (password === ""){
-    document.getElementById('charCheck').style.color = 'rgb(100 116 139)'
-}else if(password.length >= 8){
-    document.getElementById('charCheck').style.color = 'rgb(132,204,22)'
-}else{
-    document.getElementById('charCheck').style.color = 'rgb(185,28,28)'
-}
+// if (password === ""){
+//     document.getElementById('charCheck').style.color = 'rgb(100 116 139)'
+// }else if(password.length >= 8){
+//     document.getElementById('charCheck').style.color = 'rgb(132,204,22)'
+// }else{
+//     document.getElementById('charCheck').style.color = 'rgb(185,28,28)'
+// }
