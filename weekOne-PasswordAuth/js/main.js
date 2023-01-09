e874e8f74d9162fb42f95f5339234b5d33e08438
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2023-01-08 20:53:20 -0500

    project week 1 complete

--- weekOne-PasswordAuth/js/main.js
+++ weekOne-PasswordAuth/js/main.js
@@ -34,7 +34,7 @@ function passwordAuth(){
 
         // Upper case check
         if (password === ""){
-            document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
+            document.getElementById('upperCaseCheck').style.color = 'rgb(100 116 139)'
         }else if(password !== password.toLowerCase()){
             document.getElementById('upperCaseCheck').style.color = 'rgb(132,204,22)'
         }else{
@@ -65,45 +65,3 @@ function passwordAuth(){
             document.getElementById('charCheck').style.color = 'rgb(185,28,28)'
         }
 }
-// // Lower-case Check
-// if (password === ""){
-//     document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
-// }else if(password !== password.toUpperCase()){
-//     document.getElementById('lowerCaseCheck').style.color = 'rgb(132,204,22)'
-// }else{
-//     document.getElementById('lowerCaseCheck').style.color = 'rgb(185,28,28)'
-// }
-
-
-// // Upper case check
-// if (password === ""){
-//     document.getElementById('lowerCaseCheck').style.color = 'rgb(100 116 139)'
-// }else if(password !== password.toLowerCase()){
-//     document.getElementById('upperCaseCheck').style.color = 'rgb(132,204,22)'
-// }else{
-//     document.getElementById('upperCaseCheck').style.color = 'rgb(185,28,28)'
-// }
-
-// // Number Check
-
-// function containsNum(password){
-//     return /\d/.test(password)
-// }
-
-// if (password === ""){
-//     document.getElementById('numCheck').style.color = 'rgb(100 116 139)'
-// }else if(containsNum(password) === true){
-//     document.getElementById('numCheck').style.color = 'rgb(132,204,22)'
-// }else{
-//     document.getElementById('numCheck').style.color = 'rgb(185,28,28)'
-// }
-
-// // Character Count Check
-
-// if (password === ""){
-//     document.getElementById('charCheck').style.color = 'rgb(100 116 139)'
-// }else if(password.length >= 8){
-//     document.getElementById('charCheck').style.color = 'rgb(132,204,22)'
-// }else{
-//     document.getElementById('charCheck').style.color = 'rgb(185,28,28)'
-// }
